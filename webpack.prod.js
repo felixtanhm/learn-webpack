9bc006eec009213e41daf9e5241d53ab74740672
felixtanhm <felixtanhm@gmail.com>
2023-12-24 13:00:02 +0800

    Minify JS, CSS, and HTML in production - Extracted CSS into own file in production - Minified CSS in production - Added TerserJS back in to minify JS in production - Minified HTML in production as well

--- webpack.prod.js
+++ webpack.prod.js
@@ -1,14 +1,57 @@
 const path = require("path");
 const common = require("./webpack.common");
 const { merge } = require("webpack-merge");
+const HtmlWebpackPlugin = require("html-webpack-plugin");
 const { CleanWebpackPlugin } = require("clean-webpack-plugin");
+const MiniCssExtractPlugin = require("mini-css-extract-plugin");
+const CssMinimizerPlugIin = require("css-minimizer-webpack-plugin");
+const TerserPlugin = require("terser-webpack-plugin");
 
 module.exports = merge(common, {
   mode: "production",
-  plugins: [new CleanWebpackPlugin()],
   output: {
     filename: "[name].[contenthash].bundle.js",
     path: path.resolve(__dirname, "dist"),
     assetModuleFilename: "assets/[name].[hash].[ext]",
+  },
+  optimization: {
+    minimizer: [
+      new HtmlWebpackPlugin({
+        template: "./src/template.html",
+        options: {
+          removeAttributeQuotes: true,
+          removeComments: true,
+          collapseWhitespace: true,
+        },
+      }),
+      new CssMinimizerPlugIin({
+        minimizerOptions: {
+          preset: [
+            "default",
+            {
+              discardComments: { removeAll: true },
+            },
+          ],
+        },
+      }),
+      new TerserPlugin(),
+    ],
+  },
+  plugins: [
+    new HtmlWebpackPlugin({
+      template: "./src/template.html",
+    }),
+    new MiniCssExtractPlugin({
+      filename: "[name].[contenthash].css",
+    }),
+    new CleanWebpackPlugin(),
+  ],
+  module: {
+    rules: [
+      {
+        test: /\.scss$/,
+        use: [MiniCssExtractPlugin.loader, "css-loader", "sass-loader"],
+      },
+    ],
   },
 });
